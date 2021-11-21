024dda2991c70a83655455a8839a732059027f05
Alejandro <silvestroalejandro@gmail.com>
2021-11-21 16:33:09 -0300

    Se agrega iconos a BottomTabNavigator

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
+import { NativeBaseProvider } from 'native-base';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigations/Navigation';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Navigation />
-    </View>
-  );
+	return (
+		<NativeBaseProvider>
+			<View style={styles.container}>
+				<Navigation />
+			</View>
+		</NativeBaseProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
 });
--- navigations/HomeTabBottom.js
+++ navigations/HomeTabBottom.js
@@ -6,22 +6,76 @@ import HomeScreen from '../screens/HomeScreen';
 import MyListStack from './MyListStack';
 import SearchCityScreen from './../screens/SearchCityScreen';
 import AboutScreen from './../screens/AboutScreen';
+import { Icon } from 'native-base';
+import { MaterialCommunityIcons} from "@expo/vector-icons"
 
 const Tab = createBottomTabNavigator();
 
 const HomeTabBottom = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+    <Tab.Navigator
+          screenOptions={ ({route}) => ({
+            tabBarActiveTintColor: '#00a680',
+            tabBarInactiveTintColor: 'gray',
+            tabBarIcon: ({ color, size}) => {
+              let iconName='';
+
+              // Por cada valor en un tab asigno un icono
+              switch (route.name) {
+                case 'HomeScreen':
+                  iconName = 'home-outline'
+                  break;
+                  case 'MyList':
+                    iconName = 'map-search'
+                    break;
+                case 'SearchScreen':
+                  iconName = 'map-marker-plus-outline'
+                  break;
+                case 'AboutScreen':
+                  iconName = 'folder-information-outline'
+                  break;
+
+                default:
+                  break;
+              }
+
+              // Retorna el icono según la ruta
+              return <Icon as={MaterialCommunityIcons}
+                        name={iconName} size={25} color={color}
+                      />;
+            },
+          })
+        }
+        >
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          title: 'Home'
+        }}
+        />
       <Tab.Screen
         name="MyList"
         component={MyListStack}
         options={{
           headerShown: false,
+          title: 'Mis Ciudades'
         }}
       />
-      <Tab.Screen name="SearchScreen" component={SearchCityScreen} />
-      <Tab.Screen name="AboutScreen" component={AboutScreen} />
+      <Tab.Screen
+        name="SearchScreen"
+        component={SearchCityScreen}
+        options={{
+          title: 'Buscar Ciudad'
+        }}
+        />
+      <Tab.Screen
+        name="AboutScreen"
+        component={AboutScreen}
+        options={{
+          title: 'Acerca de...'
+        }}
+        />
     </Tab.Navigator>
   );
 };
